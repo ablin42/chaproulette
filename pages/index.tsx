df5147f9580a090320878cd1c261550c67ff62c7
ablin <space6fic@gmail.com>
2022-05-26 23:24:28 +0200

    Added fontawesome, footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Wheel from "../components/Wheel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGithub,
+  faEthereum,
+} from "@fortawesome/free-brands-svg-icons";
 
 const API_HOST = "https://europe.api.riotgames.com";
 const API_KEY = process.env.API_KEY;
@@ -63,7 +70,6 @@ const Home: NextPage = ({ gameData, fetchedSummoner }: any) => {
   const [usableData, setUsableData] = useState(gameData);
   const [queueType, setQueueType] = useState("430");
 
-
   useEffect(() => {
     onSearch();
   }, [queueType]);
@@ -95,7 +101,7 @@ const Home: NextPage = ({ gameData, fetchedSummoner }: any) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.main} style={{ padding: "30px 0 0 0" }}>
         <h1 className={styles.title}>"{summoner.name}"</h1>
         <br />
 
@@ -132,11 +138,65 @@ const Home: NextPage = ({ gameData, fetchedSummoner }: any) => {
           </select>
         </div>
         <Wheel items={usableData} />
+
+        <footer>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <a href="https://twitter.com/0xharb">
+              <Image width={50} height={50} src="/img/ratirlKissMirror.png" />
+            </a>
+            <a href="https://twitter.com/Chap_GG">
+              <Image width={50} height={50} src="/img/chap.png" />
+            </a>
+            <a href="https://www.instagram.com/jjdr___/">
+              <Image width={50} height={50} src="/img/ratirlKiss.png" />
+            </a>
+          </div>
+          {/* <div style={{ display: "flex", justifyContent: "center" }}>
+            <ul>
+              <li>
+                <a href="https://twitter.com/0xharb">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    fontSize={30}
+                    color="#456cd1"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/ablin42">
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    fontSize={30}
+                    color="#456cd1"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon
+                    icon={faEthereum}
+                    fontSize={30}
+                    width={30}
+                    color="#456cd1"
+                  />
+                </a>
+              </li>
+            </ul>
+            </div> */}
+
+            {/* <span>
+              <FontAwesomeIcon
+                icon={faEthereum}
+                fontSize={30}
+                color="#456cd1"
+              />{" "}
+              (ETH/ERC-20): 0xCC61d2bb1A215f19922eCF81613bEa3253713371
+            </span> */}
+        </footer>
       </main>
     </div>
   );
 };
-
 export async function getStaticProps() {
   const chap_name = "chap fill acc";
   let res = await fetch(
